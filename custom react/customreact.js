5c01c136f3b09bdee54e65f493d1c8daa9dd042a
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2026-01-10 14:29:14 +0000

    custom react

---
+++ custom react/customreact.js
@@ -0,0 +1,22 @@
+function customRender(reactElement,conatiner){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHtml = reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+
+    conatiner.appendChild(domElement)
+}
+
+const reactElement = {
+    type:'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    Children: 'click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement,mainContainer)
+
