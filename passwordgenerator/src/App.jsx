93922e4ce1d23755593883dbe41015555463bb4e
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2026-01-11 20:12:40 +0000

    removed app.css

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 
 function App() {
   const [length,setLength] = useState(8)
-  const [numberAllowed,setNumberAllowed] = useState(flase)
-  const [charAllowed,setCharAllowed] = useState(flase)
+  const [numberAllowed,setNumberAllowed] = useState(false)
+  const [charAllowed,setCharAllowed] = useState(false)
   const [password,setPassword] = useState("")
 
   const passwordGenerator = useCallback(() => {
@@ -15,20 +15,28 @@ function App() {
     if(numberAllowed) str += "0123456789"
     if(charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    for (let i = 1; i < array.length; i++) {
+    for (let i = 1; i < length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
       pass = str.charAt(char)
     }
 
     setPassword(pass)
-    
-
 
   },[length,numberAllowed,charAllowed,setPassword])
 
   return (
     <>
-    <h1 className='text-4xl text-center  text-white'>password Generator</h1>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+      <div className='className="flex shadow rounded-lg overflow-hidden mb-4"'>
+        <input
+        type="text"
+        value = {password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='password'
+        readOnly
+        />
+      </div>
+    </div>
     </>
   )
 }
