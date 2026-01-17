831bf4c35c8676e4e8b337bae4c5b9183350f24b
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2026-01-17 19:22:57 +0000

    todoapp created

--- reduxtoolkittodo/src/App.jsx
+++ reduxtoolkittodo/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 function App() {
 
@@ -9,7 +11,7 @@ function App() {
     <>
       <h1>learn about redux toolkit</h1>
       <AddTodo/>
-      <Todo/>
+      <Todos/>
     </>
   )
 }
